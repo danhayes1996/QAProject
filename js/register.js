1a7ca7444ada78c1dfc41071ca37641278433ed9
Dan Hayes <dan.hayes1996@gmail.com>
2019-07-30 16:39:59 +0100

    adding features to game page, register constraints

--- js/register.js
+++ js/register.js
@@ -1,15 +1,16 @@
-function signup(){
+function signup() {
     const inputs = document.getElementsByClassName('registerInput');
     const data = {};
-    for(input of inputs){
+    for(input of inputs) {
         data[input.id] = input.value;
     }
-    console.log(data);
+    //console.log(data);
 
     makeRequest('POST', 'http://localhost:8080/GamesApp/api/user/create', JSON.stringify(data))
         .then(value => {
             if(value.error){
-                console.log(value);
+                // console.log(value);
+                clearErrorDiv();
                 const errDiv = document.getElementById('registerError');
                 const p = document.createElement('p');
                 p.innerText = value.message;
@@ -21,4 +22,32 @@ function signup(){
         });
 
     return false;
+}
+
+function checkPasswords() {
+    const pass = document.getElementById('password');
+    const cpass = document.getElementById('cpassword');
+
+    const submitBtn = document.getElementById('submitBtn');
+    const errDiv = document.getElementById('registerError');
+    if(pass.value !== cpass.value) {
+        clearErrorDiv();
+        const p = document.createElement('p');
+        p.innerText = "Your passwords do not match.";
+        errDiv.append(p);
+        errDiv.style.display = "block";
+
+        submitBtn.disabled = true;
+    } else {
+        clearErrorDiv();
+        errDiv.style.display = "none";
+        submitBtn.disabled = false;
+    }
+}
+
+function clearErrorDiv() {
+    const errDiv = document.getElementById('registerError');
+    while(errDiv.lastChild) {
+        errDiv.removeChild(errDiv.lastChild);
+    }
 }
